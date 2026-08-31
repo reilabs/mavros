7d92a5640aa3d1ebaab4d2249954f4f9ba7d74f9
veljkovranic <byblos94@gmail.com>
2026-08-31 18:04:55 +0200

    ecdsa_secp256k1

--- compiler/src/project.rs
+++ compiler/src/project.rs
@@ -49,6 +49,10 @@ const MAVROS_STDLIB_FILES: &[(&str, &str)] = &[
         include_str!("../../mavros_stdlib/replacements/derive_pedersen_generators.nr"),
     ),
     (
+        "std/mavros/replacements/ecdsa_secp256k1.nr",
+        include_str!("../../mavros_stdlib/replacements/ecdsa_secp256k1.nr"),
+    ),
+    (
         "std/mavros/replacements/ecdsa_secp256r1.nr",
         include_str!("../../mavros_stdlib/replacements/ecdsa_secp256r1.nr"),
     ),
@@ -73,6 +77,10 @@ const MAVROS_STDLIB_FILES: &[(&str, &str)] = &[
         include_str!("../../mavros_stdlib/vendor.nr"),
     ),
     (
+        "std/mavros/vendor/ecdsa_secp256k1.nr",
+        include_str!("../../mavros_stdlib/vendor/ecdsa_secp256k1.nr"),
+    ),
+    (
         "std/mavros/vendor/ecdsa_secp256r1.nr",
         include_str!("../../mavros_stdlib/vendor/ecdsa_secp256r1.nr"),
     ),
@@ -90,6 +98,7 @@ const MAVROS_STDLIB_FILES: &[(&str, &str)] = &[
 const FOREIGN_REPLACEMENTS: &[&str] = &[
     "blake3",
     "derive_pedersen_generators",
+    "ecdsa_secp256k1",
     "ecdsa_secp256r1",
     "embedded_curve_add",
     "multi_scalar_mul",
